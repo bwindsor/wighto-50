97fee235ecbb7e2e7343fc85ffcf39400beadb87
Ben Windsor <ben.windsor@cityscience.com>
2026-08-29 19:35:48 +0100

    Add QR code generation for wifi local results

--- python/qr_codes.py
+++ python/qr_codes.py
@@ -20,3 +20,7 @@ def create_qr_code(url, output_file):
 
 
 create_qr_code("https://racesignup.co.uk/entry/?eventid=5990", "entries.png")
+create_qr_code("WIFI:T:nopass;S:wight50;;", "results_wifi.png")
+create_qr_code("http://192.168.0.100", "local_wifi_results.png")
+create_qr_code("https://www.wighto.org.uk/wp-content/uploads/2026/09/needles-and-knolls-results/day1/", "day1_online_results.png")
+create_qr_code("https://www.wighto.org.uk/wp-content/uploads/2026/09/needles-and-knolls-results/day2/", "day2_online_results.png")
